95c567e4c1cef434d617c022fe7082d79458ee81
gab <19020303@myrp.edu.sg>
2021-05-06 14:22:07 +0800

    done second activity

--- app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
+++ app/src/main/java/com/myapplicationdev/android/p04_revisionnotes/SecondActivity.java
@@ -1,18 +1,37 @@
 package com.myapplicationdev.android.p04_revisionnotes;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+
 
 public class SecondActivity extends AppCompatActivity {
+
+	ListView lvNotes;
+	ArrayList<Note> alNote;
+	RevisionNotesArrayAdapter rnaa;
+//	DBHelper dbNote;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		//TODO implement the Custom ListView
 
+		lvNotes = findViewById(R.id.lv);
+		DBHelper dbNote = new DBHelper(SecondActivity.this);
+
+		//add database values into arraylist
+		alNote = dbNote.getAllNotes();
+		rnaa = new RevisionNotesArrayAdapter(this, R.layout.row,alNote);
+		lvNotes.setAdapter(rnaa);
+
 	}
+
+
 
 
 }
